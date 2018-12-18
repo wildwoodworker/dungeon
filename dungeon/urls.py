0d31c40cd3671615e05bda54f7feeb1845fee497
Martin Frechette <martin.frechette@gmail.com>
2018-12-17 20:40:51 -0800

    update template

--- dungeon/urls.py
+++ dungeon/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 from django.conf.urls import url
+from django.urls import path
 from dungeon import views
 
 urlpatterns = [
-  url(r'^admin/', admin.site.urls),
   url(r'^$', views.home, name='home'),
   #path('room/<int:room_id>/', views.room, name='room'),
   path('nextroom', views.nextroom, name='nextroom'),
